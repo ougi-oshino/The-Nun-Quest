52239e7177f0c792ac0af48aa04c006a30e6fd0b
Erik <81354428+s0mt0x64@users.noreply.github.com>
2024-01-13 00:53:54 +0000

    added file
    entirely restructured story lines and removed bloat

--- main.py
+++ main.py
@@ -40,19 +40,19 @@ def intro():
 def start():
     print('Slutty Quest', font='caligraphy''green', attrs=['bold'])
     
-    print ("After a drunken night out with friends, you awaken the ")
-    print("next morning in a giant , abandonend church in a forest.")
+    print ("After a drunken night out fantasing about your waifu, you awaken the ")
+    print("next morning in an abandonend house in a forest.")
     time.sleep(1)
-    print("Head spinning and fighting the urge to vomit, you stand and marvel at your new, ")
+    print("Head spinning and struggling to remember, you stand and marvel at your new, ")
     print("unfamiliar setting. The peace quickly fades when you hear a ")
     time.sleep(1)
-    print("cute sound emitting behind you. A cute nun is ")
+    print("cute voice emitting behind you. A cute girl with small nice boobs enough to breastfeed you for life is ")
     print("sleeping towards you. You will:")
     time.sleep(1)
-    print ("""  A. Wake her and talk to her 
-                B. Pick her up and run to your house (pick this for true ending and a very nice sex scene)
-                C. Rape her right there""")
-    choice = input(">>> ") #Here is your first choice.
+    print ("""  A. Wake her up and talk to her 
+                B. Pick her up and run to your house 
+                C. Rape her right there like the coward you are""")
+    choice = input(">>> ") # kys please
     if choice in reply_A:
         option_wake()
     elif choice in reply_B:
@@ -65,17 +65,17 @@ def start():
          
 def option_house():
    time.sleep(1)
-   print ("\nThe nun gets startled by getting picked up suddenly, As she was about to scream, "
-   "you see a guard. Will you:")
+   print ("\nThe girl gets startled by getting picked up suddenly, As she was about to scream, "
+   "you see a huge medieval vikling dude with an axe. Will you:")
   
    print ("""A. make her shut up and hide
-             B. Put her in the ground, ask her to stay calm and make she follow you (pick this for true ending and a very nice sex scene)
-             C. Run towards a nearby alley""")
+             B. Leave her behind and hide like a coward
+             C. Run towards a nearby alley with her""")
    choice = input(">>> ")
    if choice in reply_A:
     option_hide()
    elif choice in reply_B:
-    option_calm()
+    option_coward()
    elif choice in reply_C:
     option_alley()
    else:
@@ -96,25 +96,25 @@ def option_alley():
     condom = 0
   print ("\nWhat do you do next?")
   
-  print ("""  A. Hide in silence
-              B. Rape her!
-              C. Run""")
+  print ("""  A. Ask her to have sex with her
+              B. Rape her like a degenerate!
+              C. Run away from her like an incel""")
   choice = input(">>> ")
   if choice in reply_A:
-    print ("\nYou hide in dark, the guard that heard the nun's scream "
+    print ("\nYou hide in dark, the medieval vikling dude that heard the girl's scream "
             "pass by, but "
-            "Another guard notices you there, so...\n\nYou were Caught!")
+            "Another medieval vikling dude notices you there, so...\n\nYou were Caught!")
     option_caught()
   elif choice in reply_B:
    if condom > 0:
     print ("\nAt this point you just don't care about anything anymore "
-           "you just want to rape that nun! \n\nYou are a Rapist!")
-    option_rapist()
+           "you just want to sex this girl! \n\nYou are a horny bastard!")
+    option_horny()
    else: #If the user didn't grab the condom
-     print ("\nYou should have picked up that condom. She's was possesed by a witch! "
-            "\n\nYou died!")
+     print ("\nYou should have picked up that condom. She's was horny af and would love to give you the most awesome blow job! "
+            "\n\nYou died like a pig!")
   elif choice in reply_C:
-    print ("You continue to run with the nun and then you find a way out.")
+    print ("You continue to run with the girl and then you find a way out.")
     option_town()
   else:
     print (required)
@@ -128,7 +128,7 @@ def option_town():
   time.sleep(1)      
   print("but miss. You try to calm your heavy breathing as you hide ")
   time.sleep(1)       
-  print("behind a delapitated building, waiting for the guards to come ")
+  print("behind a delapitated building, waiting for the dudes to come ")
   time.sleep(1)       
   print("charging around the corner. You notice a purple dildo ")
   time.sleep(1)      
@@ -141,80 +141,80 @@ def option_town():
   print ("You hear a group's heavy footsteps and ready yourself for the group of guards.")
   time.sleep(1)
   if dildo > 0:
-    print ("\nYou quickly sticks the purple dildo , into the")
+    print ("\nYou quickly sticks the purple dildo , into your")
     time.sleep(1)       
-    print("nun's vagina hoping it have will some magic effect. It does! The guards were raped by succubi-like angels")
+    print("your arse like a faggot")
     time.sleep(1)       
-    print("\nThey were sucked dry.")
+    print("\nYou notice your arse bleeding.")
     time.sleep(1)       
-    print("\n\nThis got weird, but you survived!")
+    print("\n\nThis got weird, but you destroyed your butt hole!")
   else: #If the user didn't grab the sword
-     print ("\nMaybe you should have picked up the dildo. "
-     "\n\nYou were caught!")
-     option_caught()
+     print ("\nMaybe you should have sex the girl. "
+     "\n\nYou regret being born!")
+     option_loser()
     
-def option_caught():
+def option_loser():
     time.sleep(1)
-    print ("\nThe guards catch and you are senteced to prison with rapist orcs in your cell!\n\nASS RAPED ENDING")
+    print ("\nThe dudes catch and you are senteced to prison with faggots in your cell!\n\nButthole RAPED ENDING")
     
-def option_rapist():
+def option_sex():
     time.sleep(1)
-    print ("\nYou enjoyed to see that defenseless nuns crying for help ")
+    print ("\nYou enjoyed an awesome sex with the girl and she begs you for more ")
     time.sleep(1)
     print("\nWhile you were piercing your big cock trough her vagina... ")
     time.sleep(1)
-    print("\nWhat?! Your condom starts to glow... The nun starts to divide ")
+    print("\nWhat?! Your condom starts bursts... The girl starts to morphs ")
     time.sleep(1)
-    print("\nInto two?! She was possed by a witch! You kill the witch ")
+    print("\nInto a snake?! She turns into a giant snake like a chinese dragon! You had no option but to kill the snake ")
     time.sleep(1)
-    print("\nThe nun thanks you and becomes your wife \n\nHERO ENDING SOMEHOW!?")
+    print("\nThe girl thanks you for saving her and becomes your wife \n\nHERO ENDING SOMEHOW!?")
     
-def option_rape():
+def option_sex():
     time.sleep(1)
-    print ("You silently sticks your dick inside the nun's pussy... ")
+    print ("You silently sticks your dick inside the girl's sweetass vagina... ")
     time.sleep(1)     
     print("She seems to enjoy it, you keep fucking her more and more THEN!")
     time.sleep(1)       
-    print("She tranforms into a WITCH! Scared you try to get your dick away ")
+    print("She tranforms into fox! Scared you try to get your dick away ")
     time.sleep(1)       
     print("But she doesn't let you... You are almost there! ")
     time.sleep(1)       
-    print("The witch starts to moan bizarrely, you CUM! ")
+    print("The fox starts to moan bizarrely, you CUM! ")
     time.sleep(1)       
-    print("The witch gets killed by a nearby bishop from ")
+    print("The fox gets killed by a nearby exorcist from ")
     time.sleep(1)       
-    print("the same church as the nun's you were raping.")
+    print("the same abandoned house as the girl you were sexing.")
     time.sleep(1)
-    print("\nGOD LOVES YOU AFTER ALL ENDING\n\n!")
+    print("\nPlease kys\n\n!")
     
 def option_wake():
     time.sleep(1)
-    print("\nYou ask the nun to see if she's okay")
+    print("\nYou ask the girl to see if she's okay")
     print("She reply 'yes' \n\n")
     time.sleep(1)
-    print("\nYou take her to her church and became "
-          "a church member.")
+    print("\nYou take her to her church and wed her "
+          "You are officially married to her.")
     time.sleep(1)
-    print("CHRISTIAN ENDING")
+    print("You sex her for the rest of your life")
     
 def option_hide():
     time.sleep(1)
-    print("\nAs you were hiding with her, you rethink your actions ")
+    print("\nAs you were hiding with her, you rethink your actions like the coward you are ")
     time.sleep(1)
-    print("You tell the nun your original intentions and asks for forgiveness.")
+    print("You tell the girl your original intentions and asks for forgiveness like a simp.")
     time.sleep(1)       
-    print("\nShe forgives you!")
-    print("THINKER ENDING!\n\n")
+    print("\nShe hates you!")
+    print("You are such a loser!\n\n")
     
 def option_calm():
     time.sleep(1)
-    print("\nThe nun calms down and decides to follow you ")
+    print("\nThe girl calms down and decides to follow you ")
     time.sleep(1)
     print("She seems curious about where you were heading...")
     time.sleep(1)
     print("You told her that she can enter your home. "
         "After you gave her some water and food,"
-        "you make a very daring request to a nun.")
+        "you make a very daring request to her.")
     time.sleep(1) 
     print("\nShe seems shocked as you asked to have sex with her.")
     time.sleep(1)
@@ -230,15 +230,15 @@ def option_calm():
     
 def sex_intro():
     time.sleep(1)
-    print("\nAs the beautiful maiden in front of you was taking of her holy clothes,")
+    print("\nAs the girl is front of you was taking of off her clothes,")
     time.sleep(1)
     print("you couldn't do anything else but stare at her modest formed ")
     time.sleep(1)
-    print("but round breasts, her chaste and small pink")
+    print("but round soft breasts, her chaste and small pink")
     time.sleep(1)
     print("little vagina, her perfectly shapped buttocks and at her nice and meaty thighs, Your penis got hard at the")
     time.sleep(1)
-    print("thought of taking the virginity off a God Sworn woman with such a swollen and lustful body.")
+    print("thought of taking the virginity off a her with such a swollen and lustful body.")
     time.sleep(1)
     choice = input("\nContinue")
     if choice in continue_text:
@@ -251,7 +251,7 @@ def sex_blowjob():
     time.sleep(1)
     print("\nAs you ask her for a blowjob, you remember")
     time.sleep(1)
-    print("that as a nun, she hardly even knows the meaning of those words.")
+    print("that as a girl, she hardly even knows the meaning of those words.")
     time.sleep(2)
     print("After you explained she obliged.")
     choice = input("\nContinue")
@@ -263,9 +263,9 @@ def sex_blowjob():
     
 def blowjob():
     time.sleep(1)
-    print("\nShe gently started licking the back of your")
+    print("\nShe gently started sucking your")
     time.sleep(1)
-    print("cock, you told her which part of the glands, she should lick to make you feel good.")
+    print("cock, you told her which part of your balls, she should lick to make you feel good.")
     time.sleep(1)
     print("She then out of nowhere started at boobjob!")
     time.sleep(1)
@@ -305,7 +305,7 @@ def sex():
     time.sleep(1)
     print("As both you were finally cumming, you promissed")
     time.sleep(1)
-    print("to marry her and make sure, she didn't profaned her status as a nun in vain.")
+    print("to marry her and make sure, she didn't profaned her status as a virgin in vain.")
     time.sleep(1)   
     print("BOTH OF YOU CUM AND KISS HAPPILY IN LOVE!")
     choice = input("\nContinue")
@@ -319,7 +319,7 @@ def true_ending():
     time.sleep(1)
     print("\nAfter that you were going to get married.")
     time.sleep(1)
-    print("She told that she wanted the Mother Superior Nun's forgiveness and approval.") 
+    print("She told that she wanted God's forgiveness.") 
     print("You agreed")
     time.sleep(1)
     print("\nTHEN YOU GOT MARRIED AND GOT CHILDREN"
@@ -333,9 +333,8 @@ def true_ending():
     
 def true():  
   time.sleep(1)
-  print("\nTRUE ENDING!!!\n\n")
+  print("\nIt does not end here!!!\n\n")
   time.sleep(1)
   print("Thanks for Playing!")
-  print("Made by S0mt0chukwu.")
-  print("Made by Ougi-Oshino.")
+  print("Made by Erik.")
     
